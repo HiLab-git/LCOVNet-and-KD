57a33f2ba27c2d95e61c546c62932c0b01b12f7a
afeizqf <1040704263@qq.com>
2023-06-09 14:39:10 +0800

    based on Pymic

---
+++ for_tmi/KD/run.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+python setup.py install
+python /home/data/zhaoqianfei/new_pymic/PyMIC/pymic/net_run/train.py /home/data/zhaoqianfei/new_pymic/for_tmi/KD/config/kd.cfg
---
+++ for_tmi/LCOVNet/run.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+python /home/data/zhaoqianfei/new_pymic/PyMIC/pymic/net_run/train.py /home/data/zhaoqianfei/new_pymic/for_tmi/LCOVNet/config/lcovnet.cfg
